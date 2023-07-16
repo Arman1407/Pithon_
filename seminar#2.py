02e6e74ec7e0516761e8286cb3d13b085d864797
Dzambulov Arman <arman1407@ya.ru>
2023-07-16 15:50:25 +0300

    seminar#2

--- seminar#2.py
+++ seminar#2.py
@@ -88,22 +88,22 @@
 # длилась самая длинная оттепель. Оттепелью они называют период, в который среднесуточная температура ежедневно превышала 0 градусов Цельсия. 
 # Напишите программу, помогающую синоптикам в работе.
 
-from random import randint
+# from random import randint
 
-days = int(input('Введите количество дней: '))
-max_len, count, temp = 0, 0, 0
+# days = int(input('Введите количество дней: '))
+# max_len, count, temp = 0, 0, 0
 
-for _ in range(days):
-    temp += randint(-3, 3)
-    print(temp, end=' ')
-    if temp > 0:
-        count += 1
-        if count > max_len:
-            max_len = count
-    else:
-        count = 0
+# for _ in range(days):
+#     temp += randint(-3, 3)
+#     print(temp, end=' ')
+#     if temp > 0:
+#         count += 1
+#         if count > max_len:
+#             max_len = count
+#     else:
+#         count = 0
 
-print(f'\nМаксимальное количество теплых дней подряд - {max_len} дней')
+# print(f'\nМаксимальное количество теплых дней подряд - {max_len} дней')
 
 
 
