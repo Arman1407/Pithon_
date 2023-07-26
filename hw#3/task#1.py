8039f8c62b280d55fb3dc46ef8a41ee971cb7ae3
Arman_Dzhambulov <arman1407@ya.ru>
2023-07-26 15:30:50 +0300

    task#1

---
+++ hw#3/task#1.py
@@ -0,0 +1,20 @@
+# Требуется вычислить, сколько раз встречается некоторое число k в массиве list_1.
+# Найдите количество и выведите его.
+
+# Пример:
+
+# list_1 = [1, 2, 3, 4, 5]
+# k = 3
+# 1
+
+
+from random import randint
+
+print(lst := [randint(-5, 5) for _ in range(1, 11)])
+n = int(input(f'Введите число: '))
+count = 0
+for i in lst:
+    if i == n:
+        count += 1
+print(count)
+
